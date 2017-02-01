6450b80973eebfd3013cf6941f2eec9670e2378f
AL <amanwitheyes@gmail.com>
2017-01-31 22:34:45 -0500

    first commit

---
+++ routes/tasks.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://moustique:moustique@ds151048.mlab.com:51048/mytaskslist_moustique', ['tasks']);
+
+
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err, tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks); 
+    })
+});
+
+router.get('/task/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task); 
+    })
+});
+
+router.post('/task', function(req, res, next){
+    var tasks = req.body;
+    if(!task.title || (task.isDone + '')){
+        res.status(400);
+        res.json({"error":"Bad data"});
+    }else{
+        db.tasks.save(task, function(err, task){
+            if(err){
+                res.send(err);
+            }
+            res.json(tasks); 
+        });
+    }
+
+});
+
+
+router.delete('/task/:id', function(req, res, next){
+    db.tasks.remove({_id: mongojs.ObjectId(req.params.id)},function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task); 
+    })
+});
+
+router.put('/task/:id', function(req, res, next){
+    var task = req.body;
+    var updateTask = {};
+
+    if(task.isDone){
+        updateTask.isDone = task.isDone;
+    }
+    if(task.title){
+        updateTask.title = task.title;
+    }
+
+    if(!updateTask){
+        res.status(400);
+        res.json({"error":"Bad data"});
+    }else{
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)},updateTask,{},function(err, task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task); 
+        })
+    }
+
+});
+
+
+module.exports = router;
